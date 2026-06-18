db4565116c4607a132d37da26cb3244afba849c8
pranaypansara <pansarapranay18@gmail.com>
2026-06-18 23:24:59 +0530

    useEffect practice

--- Week 19/src/App.tsx
+++ Week 19/src/App.tsx
@@ -1,35 +1,68 @@
+//UseEffect: body, dependency array, cleanup function inside bodyg
 import { APITester } from "./APITester";
 import axios from "axios";
 import { useState, useEffect } from "react";
 
 export function App() {
-  let [data, setData] = useState([]);
-
-  //if the dependency array (2nd argument) is empty
-  //then this function runs only on the first render (mount)
-  useEffect(function () {
-    axios
-      .get("https://jsonplaceholder.typicode.com/todos/")
-      .then((response) => {
-        setData(response.data);
-      });
-  }, []);
-
+  let [currentid, setcurrentid] = useState([]);
   return (
     <div className="app">
-      {data.map((todo) => (
-        <Todo title={todo.title} />
-      ))}
+      <button
+        onClick={function () {
+          setcurrentid(1);
+        }}
+      >
+        1
+      </button>
+      <button
+        onClick={function () {
+          setcurrentid(2);
+        }}
+      >
+        2
+      </button>
+      <button
+        onClick={function () {
+          setcurrentid(3);
+        }}
+      >
+        3
+      </button>
+      <Todo id={currentid} />
     </div>
   );
 }
 
-function Todo(props: { title: string }) {
-  return (
-    <div style={{ margin: 10, border: "2px solid black" }}>
-      <div>{props.title}</div>
-    </div>
+function Todo(props: any) {
+  let [data, setdata] = useState("");
+  const id = props.id;
+
+  //if the dependency array (2nd argument) is empty
+  //then this function runs only on the first render (mount)
+  //if we want to re-render useEffect everytime id changes
+  //then we have to add id in the dependency array
+  useEffect(
+    function () {
+      axios
+        .get("https://jsonplaceholder.typicode.com/todos/" + id)
+        .then((res) => {
+          setdata(res.data.title);
+        });
+
+      //example to show usecase of cleanup function
+      let clockid = setInterval(() => {
+        console.log("hi there " + id);
+      }, id * 1000);
+
+      //ts cleanup function is triggered everytime before rerendering whenever a dependency changes (id in this case)
+      return function () {
+        clearInterval(clockid);
+      };
+    },
+    [id],
   );
+
+  return <div style={{ margin: 10, border: "2px solid black" }}>{data};</div>;
 }
 
 export default App;
